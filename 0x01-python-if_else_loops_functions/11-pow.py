bdcffe6714323f9444536c1a0e9a36affd084598
jayeoba paul <tunmisejayeoba@gmail.com>
2023-08-14 03:45:38 +0000

    My python if else loop function file

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Jayoba tunmise
+
+def pow(a, b):
+    return (a ** b)
---
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -0,0 +1,6 @@
+
+#!/usr/bin/python3
+#!cse = __import__('8-uppercase').uppercase
+
+uppercase("best")
+uppercase("Best School 98 Battery street")usr/bin/python3
